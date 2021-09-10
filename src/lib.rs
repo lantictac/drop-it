1be935608daa277b99e696c7ec6fff77d0efc7e0
Juan Lledo <juan.lledo@hotmail.com>
2021-09-10 23:56:24 +0200

    Add IPv6 support

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use crate::server::Server;
 use crate::service_event_handler::ServiceEventHandler;
 
 mod server;
@@ -6,7 +7,8 @@ mod service_event_handler;
 pub fn run(service_name: &str) -> Result<(), anyhow::Error> {
     let sys = actix_web::rt::System::new(service_name);
 
-    server::run()?;
+    const PORT: u16 = 6916;
+    Server::new(PORT).run()?;
 
     ServiceEventHandler::new(service_name).run()?;
 
