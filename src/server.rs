0bb052445e39ee14583e9876c3543a7173de0f9b
Will Wilson <will@indefiant.com>
2024-02-01 12:52:30 +0800

    Allow optional shutdown parameters for message, timeout_s and force_close_apps.

--- src/server.rs
+++ src/server.rs
@@ -1,21 +1,37 @@
+use actix_web::web::Query;
+use serde::Deserialize;
 use std::io;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
 use system_shutdown::shutdown_with_message;
 
 use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
+#[derive(Deserialize)]
+struct ShutdownParams {
+    message: Option<String>,
+    timeout_s: Option<u32>,
+    force_close_apps: Option<bool>,
+}
+
 #[get("/shutdown")]
-async fn shutdown(req: HttpRequest) -> impl Responder {
+async fn shutdown(req: HttpRequest, params: Query<ShutdownParams>) -> impl Responder {
     let connection_info = req.connection_info();
     let request_addr = match connection_info.realip_remote_addr() {
         Some(addr) => addr,
         None => "<unknown>",
     };
 
+    let user_message = match params.message.as_ref() {
+        Some(message) => message,
+        None => "No reason"
+    };
+
+    let message = format!("Drop-It shutdown requested by {request_addr}: {user_message}");
+
     match shutdown_with_message(
-        format!("Drop-It shutdown invoked remotely by {request_addr}").as_str(),
-        10,
-        false
+        &message,
+        params.timeout_s.unwrap_or(10),
+        params.force_close_apps.unwrap_or(false),
     ) {
         Ok(_) => HttpResponse::Ok().body("Bye!"),
         Err(_error) => HttpResponse::InternalServerError().into(),
